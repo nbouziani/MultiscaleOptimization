5938ba6ac4aeef6ac3a0911dab6b8bd8997998c9
nbouziani <nacime.bouziani@gmail.com>
2022-10-17 22:38:37 +0100

    Update __init__.py

--- external_operators/__init__.py
+++ external_operators/__init__.py
@@ -1,1 +1,2 @@
-from external_operators.PDE_operator import PDEOperator, pde_operator
+from external_operators.pde_operator import PDEOperator, pde_operator
+from external_operators.multiscale_operator import MultiscaleOperator, multiscale_operator
