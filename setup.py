a618b8039603734cc5c9c75c1fa0737148bb10e9
nbouziani <48448063+nbouziani@users.noreply.github.com>
2023-01-09 00:11:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="multiscale_optimization",
     version="1.0",
+    author="Nacime Bouziani",
+    author_email="n.bouziani18@imperial.ac.uk",
     packages=find_packages(),
 )
